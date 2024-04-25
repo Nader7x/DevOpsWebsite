7c4ab7742d155c134d6a316288132ffb1830c5c7
MazenMustafa1 <104434230+MazenMustafa1@users.noreply.github.com>
2024-04-25 21:56:36 +0200

    Initial Migration

---
+++ Opsphere.Data/Migrations/20240425194258_Initial.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Opsphere.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Developers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Developers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamLeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamLeaders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamLeaderId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                    table.ForeignKey(
+                        name: "FK_Projects_TeamLeaders_TeamLeaderId",
+                        column: x => x.TeamLeaderId,
+                        principalTable: "TeamLeaders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: true),
+                    TeamLeaderId = table.Column<int>(type: "int", nullable: true),
+                    DeveloperId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardId);
+                    table.ForeignKey(
+                        name: "FK_Cards_Developers_DeveloperId",
+                        column: x => x.DeveloperId,
+                        principalTable: "Developers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Cards_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId");
+                    table.ForeignKey(
+                        name: "FK_Cards_TeamLeaders_TeamLeaderId",
+                        column: x => x.TeamLeaderId,
+                        principalTable: "TeamLeaders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeveloperProject",
+                columns: table => new
+                {
+                    DevelopersId = table.Column<int>(type: "int", nullable: false),
+                    ProjectsProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeveloperProject", x => new { x.DevelopersId, x.ProjectsProjectId });
+                    table.ForeignKey(
+                        name: "FK_DeveloperProject_Developers_DevelopersId",
+                        column: x => x.DevelopersId,
+                        principalTable: "Developers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeveloperProject_Projects_ProjectsProjectId",
+                        column: x => x.ProjectsProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_DeveloperId",
+                table: "Cards",
+                column: "DeveloperId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_ProjectId",
+                table: "Cards",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_TeamLeaderId",
+                table: "Cards",
+                column: "TeamLeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeveloperProject_ProjectsProjectId",
+                table: "DeveloperProject",
+                column: "ProjectsProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_TeamLeaderId",
+                table: "Projects",
+                column: "TeamLeaderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "DeveloperProject");
+
+            migrationBuilder.DropTable(
+                name: "Developers");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "TeamLeaders");
+        }
+    }
+}
