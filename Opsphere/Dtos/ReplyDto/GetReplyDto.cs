ee8d07b29d865af387eae692c9a10c8c074f1449
MazenMustafa1 <104434230+MazenMustafa1@users.noreply.github.com>
2024-05-10 23:07:23 +0300

    Add missing files

---
+++ Opsphere/Dtos/ReplyDto/GetReplyDto.cs
@@ -0,0 +1,10 @@
+﻿using Opsphere.Dtos.User;
+
+namespace Opsphere.Dtos.ReplyDto;
+
+public class GetReplyDto
+{
+    public string? ReplyContent { get; set; }
+
+    public Data.Models.User? User { get; set; }
+}
---
+++ Opsphere/Dtos/User/ReplyUserDto.cs
@@ -0,0 +1,10 @@
+﻿using Opsphere.Data.Models;
+
+namespace Opsphere.Dtos.User;
+
+public class ReplyUserDto
+{
+    public string? Username { get; set; }
+    public string? Email { get; init; }
+    public UserRoles Role { get; init; }
+}
