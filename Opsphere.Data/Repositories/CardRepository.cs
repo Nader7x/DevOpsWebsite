8bd386d2a7d2e754117fb65b4e3fa7b97933bc74
Nader Elnagar <105499090+Nader7x@users.noreply.github.com>
2024-05-11 04:51:27 +0300

    ...

--- Opsphere.Data/Repositories/CardRepository.cs
+++ Opsphere.Data/Repositories/CardRepository.cs
@@ -4,7 +4,6 @@ using Opsphere.Data.Models;
 
 namespace Opsphere.Data.Repositories;
 
-
 public class CardRepository(ApplicationDbContext dbContext) : BaseRepository<Card>(dbContext), ICardRepository
 {
     private readonly ApplicationDbContext _dbContext = dbContext;
@@ -16,6 +15,7 @@ public class CardRepository(ApplicationDbContext dbContext) : BaseRepository<Car
 
     public async Task<List<Card>?> GetProjectCardsAsync(int projectId)
     {
-        return await _dbContext.Cards.Where(c => c.ProjectId == projectId).ToListAsync();
+        return await _dbContext.Cards.Where(c => c.ProjectId == projectId).Include(c => c.AssignedDeveloper)
+            .ThenInclude(ad => ad.User).ToListAsync();
     }
 }
